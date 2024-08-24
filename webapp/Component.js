30111d7b18f2948629eaf6b3dc7b5377ecfdba65
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-08-24 18:07:33 -0300

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("analiseatendimentos3.analiseatendimentos3.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
